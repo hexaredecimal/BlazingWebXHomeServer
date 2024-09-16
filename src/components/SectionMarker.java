f5369d105142e2027a5da6fe147f5ebdfe1d0039
hexaredecimal <mfanakagama@gmail.com>
2024-09-16 20:12:14 +0200

    components: Adds a new section separator component

---
+++ src/components/SectionMarker.java
@@ -0,0 +1,31 @@
+package components;
+
+import webx.Div;
+import webx.WebXContainerElement;
+import webx.H3;
+
+/**
+ *
+ * @author hexaredecimal
+ */
+public class SectionMarker extends WebXContainerElement {
+	private String label; 
+
+	public SectionMarker(String label) {
+		this.label = label; 
+	}
+	
+	@Override
+	public String render() {
+		return 
+				new Div()
+					.attr(
+						"style", 
+						"background-image: url(images/newtiny.jpg);")
+					.addChildren(
+						new H3(this.label)
+					)
+					.className("p-5 w-full h-30")
+					.render();
+	}
+}
